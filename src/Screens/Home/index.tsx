4724641dc34e06c0f831c7731437368c22e267f1
danielmottaa <dev.danielmotta@gmail.com>
2023-04-27 19:42:44 -0300

    :zap: integration with api

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFonts, Roboto_300Light, Roboto_900Black } from '@expo-google-fonts/roboto';
 import StatusBar from '../../components/StatusBar';
 import Colors from '../../global/colors';
-import * as S from './styles';
 import Input from '../../components/Input';
+import * as S from './styles';
+import api from '../../services/api';
+import axios from 'axios';
 
 const Home: React.FC = () => {
 
   const [characterName, setCharacterName] = useState('');
+  const [dataHeros, setDataHeros] = useState<any[]>([]);
+
+  const apiKey = 'fd49d93aab08c57ab8cb2f23cdd95def';
+  const hackApiKey = '6066446c6e90a054540043dc3744db9b'
+
+
+  useEffect(() => {
+    const getApiData = async () => {
+      try {
+        const response = await api.get(`characters?ts=1&apikey=${apiKey}&hash=${hackApiKey}`);
+        const { data } = response.data;
+        setDataHeros(data?.results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getApiData();
+  }, []);
 
   const [fontLoaded] = useFonts({
     Roboto_300Light,
     Roboto_900Black,
   });
-  if (!fontLoaded)
-    return null;
 
-    const handleInfoHero = () => {
-      console.log('cliquei');
-    }
+  if (!fontLoaded) {
+    return null;
+  }
+
+  const handleInfoHero = () => {
+    console.log('cliquei');
+  }
+
+  const renderItem = ({ item }: any) => {
+    return (
+      <>
+        <S.CustomButton onPress={() => handleInfoHero()}>
+          <S.BoxHeroContent>
+            <S.ImageCustom source={{ uri: `${item?.thumbnail?.path}.${item?.thumbnail?.extension}`}} />
+            <S.LabelHeroName numberOfLines={1}>{item?.name}</S.LabelHeroName>
+          </S.BoxHeroContent>
+        </S.CustomButton>
+        <S.LineSeparatorHero />
+      </>
+    );
+  }
 
   return (
     <S.Container>
@@ -26,7 +62,6 @@ const Home: React.FC = () => {
       <S.ContentHeader>
         <S.LabelTilePage>BUSCA MARVEL <S.SpanTextTitlePage>TESTE FRONT-END</S.SpanTextTitlePage></S.LabelTilePage>
         <S.LineSeparator />
-
         <S.LabelCharacterName>Nome do Personagem</S.LabelCharacterName>
         <Input
           value={characterName}
@@ -37,13 +72,13 @@ const Home: React.FC = () => {
         <S.LabelName>Nome</S.LabelName>
       </S.BoxSeparatorHeader>
       <S.BoxContent>
-        <S.CustomButton onPress={() => handleInfoHero()}>
-          <S.BoxHeroContent>
-            <S.ImageCustom source={{ uri: 'https://i.em.com.br/mvwul22IjEMwZJFgWx8GppIMtoc=/1200x720/smart/imgsapp.em.com.br/app/noticia_127983242361/2021/12/16/1331553/tom-holland-como-o-homem-aranha-tem-imensas-patas-nas-costas_1_79335.jpg' }} />
-            <S.LabelHeroName numberOfLines={1}>Peter Parker</S.LabelHeroName>
-          </S.BoxHeroContent>
-        </S.CustomButton>
-        <S.LineSeparatorHero />
+        <S.FlatListCustom
+          data={dataHeros}
+          renderItem={renderItem}
+          keyExtractor={(item: any) => item.id}
+          showsVerticalScrollIndicator={false}
+       
+        />
       </S.BoxContent>
     </S.Container>
   );
